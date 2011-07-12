c92a4bb5bb73c158bfeb49fe9bab0c82bf9016a0
gabrbedd@gmail.com <gabrbedd@gmail.com@64b81567-61a9-a4b8-9b11-d1e416ee8e10>
2011-07-12 02:56:54 +0000

    process-switching: Commit an intermittent version.

--- experiments/scheduling/process-switching/process-switching.cpp
+++ experiments/scheduling/process-switching/process-switching.cpp
@@ -41,6 +41,7 @@
 using namespace std;
 
 #define SHM_NAME "/process-switching"
+#define RUN_INTERVAL 40000
 
 namespace Details
 {
@@ -61,6 +62,7 @@ namespace Details
     double usec_avg_sum;
     unsigned long usec_avg_count;
 
+#ifdef USE_LOGGER
     /* A circular buffer for the threads to log their messages
      */
     class Logger
@@ -166,6 +168,108 @@ namespace Details
         int m_read, m_write;
         std::vector<Message> m_queue;
     }; // class Logger
+#endif // USE_LOGGER
+
+    /* If b is after a, then value will be > 0
+     */
+    static double calc_dt_double(const timeval& a, const timeval& b) {
+        return (double(b.tv_sec) - double(a.tv_sec)) * 1000000.0
+            + double(b.tv_usec) - double(a.tv_usec);
+    }
+
+    static unsigned long calc_dt_unsigned(const timeval& a, const timeval& b) {
+        unsigned long ans = 0;
+        assert(a.tv_usec < 1000000);
+        assert(b.tv_usec < 1000000);
+        if( b.tv_sec > a.tv_sec ) {
+            ans = (b.tv_sec - a.tv_sec) * 1000000 + b.tv_usec - a.tv_usec;
+        } else if (b.tv_sec == a.tv_sec) {
+            if(b.tv_usec > a.tv_usec) {
+                ans = b.tv_usec - a.tv_usec;
+            }
+        }
+        return ans;
+    }
+
+    static long calc_dt_int(const timeval& a, const timeval& b) {
+        long ans;
+        assert(a.tv_usec < 1000000);
+        assert(b.tv_usec < 1000000);
+        ans = (b.tv_sec - a.tv_sec) * 1000000 + b.tv_usec - a.tv_usec;
+        return ans;
+    }
+
+    class Stats
+    {
+    public:
+        Stats() { reset(); }
+        ~Stats() {}
+
+        void new_cycle(const timeval& expected) {
+            timeval now;
+            double dt;
+            gettimeofday(&now, 0);
+            ++N_cycles;
+            dt = calc_dt_double(expected, now);
+            sum_cycle_dt += dt;
+            if(dt > max_cycle_dt)
+                max_cycle_dt = dt;
+            if(dt < min_cycle_dt)
+                min_cycle_dt = dt;
+            m_last_time = now;
+        }
+
+        void update(const Message m) {
+            double dt;
+            ++N_dt;
+            dt = calc_dt_double(m_last_time, m.time);
+            assert(dt >= 0.0);
+            sum_dt += dt;
+            if(dt > max_dt)
+                max_dt = dt;
+            if(dt < min_dt)
+                min_dt = dt;
+        }
+
+        void report() {
+            double cycle_accuracy = sum_cycle_dt / N_cycles;
+            double switch_speed = sum_dt / N_dt;
+            cout << "Cycle scheduling happened (avg) within "
+                 << cycle_accuracy << " usecs of the appointed time."
+                 << endl
+                 << "max/min = " << max_cycle_dt << "/" << min_cycle_dt
+                 << endl;
+            cout << "Thread switching speed was avg "
+                 << switch_speed << " usecs"
+                 << endl
+                 << "max/min = " << max_dt << "/" << min_dt
+                 << endl;
+        }
+
+        void reset() {
+            N_cycles = 0;
+            sum_cycle_dt = 0.0;
+            max_cycle_dt = 0.0;
+            min_cycle_dt = 0.0;
+            m_last_time.tv_sec = 0; m_last_time.tv_usec = 0;
+            N_dt = 0;
+            sum_dt = 0.0;
+            max_dt = 0.0;
+            min_dt = 0.0;
+        }
+
+    private:
+        unsigned long N_cycles;
+        double sum_cycle_dt;
+        double max_cycle_dt;
+        double min_cycle_dt;
+        timeval m_last_time;
+        unsigned long N_dt;
+        double sum_dt;
+        double max_dt;
+        double min_dt;
+
+    }; // class Stats
 
     static void output_message(const Message& m, const timeval& t_zero)
     {
@@ -186,17 +290,26 @@ namespace Details
 int main(int argc, char* argv[])
 {
     const int NPROCS = 32;
-    const int NCYCLES = 64;
+    const int NCYCLES = (1L << 16);
     const unsigned MAXMSG = 4096;
+#ifdef USE_LOGGER
     Details::Logger log(MAXMSG+1);
+#endif
     Details::Sync *proc_sync;
     Details::Sync *client_sync;
     pid_t procs[NPROCS];
     void *shm_mem;
-    timeval t_zero;
     int k,j;
 
+#ifdef USE_LOGGER
     assert( NPROCS*NCYCLES < MAXMSG );
+#endif
+
+    cout << "Running with:" << endl
+         << NPROCS << " processes" << endl
+         << NCYCLES << " cycles" << endl
+         << RUN_INTERVAL << " usec/cycle" << endl;
+    cout << "This test should take about " << (double(RUN_INTERVAL)*double(NCYCLES)/1000000.0) << " secs" << endl;
 
     // Prepares shared memory
     shm_mem = mmap(0, NPROCS * sizeof(Details::Sync), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
@@ -229,16 +342,36 @@ int main(int argc, char* argv[])
         }
     }
 
-    Details::Message m;
+    unsigned long interval;
+    long balance; //usecs
+    timeval t_zero, t_next, t_now;
+    Details::Stats stats;
 
+    interval = RUN_INTERVAL;
     Details::usec_avg_sum = 0.0;
     Details::usec_avg_count = 0;
     gettimeofday(&t_zero, 0);
+    t_next = t_zero;
     for(j=0 ; j<NCYCLES ; ++j) {
+        stats.new_cycle(t_next);
         for(k=0 ; k<NPROCS ; ++k) {
             sem_post(&proc_sync[k].run);
             sem_wait(&proc_sync[k].ret);
+#ifdef USE_LOGGER
             log.push(proc_sync[k].m);
+#endif
+            stats.update(proc_sync[k].m);
+        }
+        t_next.tv_usec += interval;
+        if(t_next.tv_usec > 100000) {
+            t_next.tv_sec += 1;
+            t_next.tv_usec -= 1000000;
+        }
+        gettimeofday(&t_now, 0);
+        balance = Details::calc_dt_int(t_now, t_next);
+        if(balance > 100) {
+            //cerr << "sleeping " << balance << endl;
+            usleep(balance);
         }
     }
 
@@ -249,14 +382,19 @@ int main(int argc, char* argv[])
 
     sleep(1);
 
+#ifdef USE_LOGGER
     while( log.pop(m) ) {
         output_message(m, t_zero);
     }
+#endif
+    stats.report();
 
+#if 0
     cout << "Average delay was "
          << (double(Details::usec_avg_sum)/double(Details::usec_avg_count))
          << " micro-seconds"
          << endl;
+#endif
     cout << "Exited cleanly" << endl;
 
     return 0;
