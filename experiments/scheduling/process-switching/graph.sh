c86cf2b43d803d33cc60551355138172ca405ca6
olivier.guilyardi@gmail.com <olivier.guilyardi@gmail.com@64b81567-61a9-a4b8-9b11-d1e416ee8e10>
2011-07-15 18:00:16 +0000

    add script to produce png graphs out of process-switching test results

---
+++ experiments/scheduling/process-switching/graph.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DATA=$1
+PNG=$2
+
+if [ -z "$DATA" ] || [ -z "$PNG" ]
+then
+    echo "Usage: `basename $0` <data.txt> <output.png>"
+    exit 1
+fi
+
+cat << EOF | gnuplot 
+set terminal png
+set output "$PNG"
+set key left top
+set key box linestyle 1
+set xlabel "Number of processes"
+set ylabel "Microseconds"
+plot '$DATA' using 1:9 title "cycle time", \
+     '$DATA' using 1:13 title "thread switch time", \
+     '$DATA' using 1:(\$9+3*\$10) title "cycle max", \
+     '$DATA' using 1:(\$13+3*\$14) title "thread switch max"
+EOF
